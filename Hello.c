02241835649d21cd8d4e74a233bad64e024f657c
Mary91 <jmarinela07@gmail.com>
2013-10-29 12:54:34 -0700

    hopefully this worked

---
+++ Hello.c
@@ -0,0 +1,12 @@
+
+
+#include <stdio.h>
+
+int main ()
+{
+printf("Hello World\n");
+}
+
+//Josephine Ntamushobora
+
+// For extra credit: The program converts a float to hexadecimal and uses And to produce the output.
