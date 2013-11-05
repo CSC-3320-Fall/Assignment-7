62cb25ada78d6d0a9e3bb91420f8352fee3c3df5
ErikThorsson <Eorndahl@gmail.com>
2013-11-04 23:36:06 -0500

    can now enter file path from cmd line

--- eorndahl1/breakCaesar.c
+++ eorndahl1/breakCaesar.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 
-int main()
+int main(int argc, char** argv)
 {
     FILE *f; //make file pointer
     int c, top = 0, count[26]={0};
     char topC, ch = 97;
     
     /* opening file for reading */
-    f = fopen("/Users/eorndahl/Assignment7/text.one.encrypt" , "r");
+    f = fopen(argv[1] , "r");
     if(f == NULL) {
         perror("Error opening file");
         return(-1);
@@ -29,22 +29,19 @@ int main()
     }
     printf("The key for this sample was %c or the right shift of %d spaces.\n\nHere is the decoded text:\n\n", topC, topC - 101);
     fclose(f);
-    f = fopen("/Users/eorndahl/Assignment7/text.one.encrypt" , "r");
+    f = fopen(argv[1], "r");
     do {
-        c = getc (f);
+        c = getc (f); //get characters one by one from input stream f
         int cha;
-            cha = c;
-            cha = (((c -97) - (topC - 101)) % 26);
-            if(cha < 0)
-                cha = 26 + cha + 97;
-            else
-                cha = cha +97;
+        cha = c;
+        cha = (((c -97) - (topC - 101)) % 26);
+        if(cha < 0)
+            cha = 26 + cha + 97;
+        else
+            cha = cha +97;
         printf("%c", (char)cha); // and print with the found shift
     } while (c != EOF);
     
     fclose(f);
     return(0);
 }
-
-
-
